777c165748207dc005068a823a155e116ac8607c
matao <matao@futurelab.com>
2025-12-19 16:15:33 +0800

    鉴权

--- backend/api/middleware/session.go
+++ backend/api/middleware/session.go
@@ -36,6 +36,7 @@ import (
 var noNeedSessionCheckPath = map[string]bool{
 	"/api/passport/web/email/login/":       true,
 	"/api/passport/web/email/register/v2/": true,
+	"/api/passport/web/ticket/login/":      true,
 }
 
 func SessionAuthMW() app.HandlerFunc {
